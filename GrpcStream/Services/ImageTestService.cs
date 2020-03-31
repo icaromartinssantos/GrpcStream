ca22d234197f5838afdb68c4b9c75cc16f65ec38
icaro martins <icaro@timais.com>
2020-03-31 14:28:06 -0300

    Add project files.

---
+++ GrpcStream/Services/ImageTestService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcStream.Services
+{
+    public static class ImageTestService
+    {
+
+        public static void StartServer()
+        {
+            Server server = new Server
+            {
+                Services = { ImageTest.BindService(new ImageTestServiceImpl()) },
+                Ports = { new ServerPort("localhost", 5001, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("RouteGuide server listening on port " + 5001);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ GrpcStream/Services/ImageTestServiceImpl.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcStream.Services
+{
+    public class ImageTestServiceImpl: ImageTest.ImageTestBase
+    {
+        public override async Task Analyse(IAsyncStreamReader<Msg> requestStream,
+                                             IServerStreamWriter<Msg> responseStream,
+                                             ServerCallContext context)
+        {
+            try
+            {
+                while (await requestStream.MoveNext())
+                {
+                    await responseStream.WriteAsync(requestStream.Current);
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Exit connection Stream");
+            }
+
+        }
+    }
+}
